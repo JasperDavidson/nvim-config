4c59d78125c4221d194c8b3226565b9f2a35938a
JasperDavidson <jasperdavdsn@gmail.com>
2026-04-30 17:55:36 -0400

    Switched to matugen (adapts to wallpaper) theme

--- plugin/00-editor-theme.lua
+++ plugin/00-editor-theme.lua
@@ -1,2 +1,14 @@
-vim.pack.add({ "https://github.com/wtfox/jellybeans.nvim" })
-vim.cmd.colorscheme("jellybeans")
+--vim.pack.add({ "https://github.com/wtfox/jellybeans.nvim" })
+--vim.cmd.colorscheme("jellybeans")
+vim.pack.add({
+	"https://github.com/daedlock/matugen.nvim",
+})
+
+require("matugen").setup({
+	colors_path = "~/.config/matugen/colors.json",
+})
+vim.cmd.colorscheme("matugen")
+
+-- Allow for transparency
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
